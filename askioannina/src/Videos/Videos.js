e09fc81a3025067d7f1e1ed33b71ef73c64aa0f8
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-11-07 14:23:35 +0200

    Loading videos from youtube api only once

--- askioannina/src/Videos/Videos.js
+++ askioannina/src/Videos/Videos.js
@@ -6,68 +6,72 @@ import Navigation from "../Router/Navigation";
 
 
 function Videos() {
-    const [allResults, setVideos] = useState([]);
 
     const API_KEY = youtube_api();
     const CHANNEL_ID = "UCVnuhNpZ6W3gSq8PhgsFz-g";
+    const [elements, setElements] = useState([]);
+
     useEffect(() => {
+
         axios.get(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=50`)
             .then((response) => {
-                setVideos(response.data.items);
+                const allResults = response.data.items
+
+
+
+        const videos = [];
+        let elements = [];
+        // deleting all unnecessary elements and keeping only videos
+        allResults.forEach((result)=>{
+            if(result.id.kind === "youtube#video"){
+                videos.push(result);
+            }
+        });
+
+        for (let i = 0; i < videos.length; i+=3) {
+            elements.push(
+                <div key={i} className="w3-row">
+                    <div key={i} className="w3-col l4">
+                        <SingleVideoComponent
+                            date={videos[i].snippet.publishedAt.split("T")[0]}
+                            link={videos[i].id.videoId}
+                            image={videos[i].snippet.thumbnails.high.url}
+                            title={videos[i].snippet.title}
+                            fullDescription={videos[i].snippet.description}
+                        />
+                    </div>
+
+                    {i+1<videos.length&&(<div key={i+1} className="w3-col l4">
+                        <SingleVideoComponent
+                            date={videos[i+1].snippet.publishedAt.split("T")[0]}
+                            link={videos[i+1].id.videoId}
+                            image={videos[i+1].snippet.thumbnails.high.url}
+                            title={videos[i+1].snippet.title}
+                            fullDescription={videos[i+1].snippet.description}
+                        />
+                    </div>)}
+
+                    {i+2<videos.length&&(
+                        <div key={i+2} className="w3-col l4">
+                            <SingleVideoComponent
+                                date={videos[i+2].snippet.publishedAt.split("T")[0]}
+                                link={videos[i+2].id.videoId}
+                                image={videos[i+2].snippet.thumbnails.high.url}
+                                title={videos[i+2].snippet.title}
+                                fullDescription={videos[i+2].snippet.description}
+                            />
+                        </div>)}
+                </div>
+            );
+        }
+
+        setElements(elements);
             })
             .catch((error) => {
-                console.error('Error fetching data:', error);
+                console.error('Error fetching data from youtube');
             });
     }, [API_KEY, CHANNEL_ID]);
 
-    const videos = [];
-
-    // deleting all unnecessary elements and keeping only videos
-    allResults.forEach((result)=>{
-        if(result.id.kind === "youtube#video"){
-            videos.push(result);
-        }
-    });
-
-
-    const elements = [];
-
-    for (let i = 0; i < videos.length; i+=3) {
-        elements.push(
-            <div key={i} className="w3-row">
-                <div key={i} className="w3-col l4">
-                    <SingleVideoComponent
-                        date={videos[i].snippet.publishedAt.split("T")[0]}
-                        link={videos[i].id.videoId}
-                        image={videos[i].snippet.thumbnails.high.url}
-                        title={videos[i].snippet.title}
-                        fullDescription={videos[i].snippet.description}
-                    />
-                </div>
-
-                {i+1<videos.length&&(<div key={i+1} className="w3-col l4">
-                    <SingleVideoComponent
-                        date={videos[i+1].snippet.publishedAt.split("T")[0]}
-                        link={videos[i+1].id.videoId}
-                        image={videos[i+1].snippet.thumbnails.high.url}
-                        title={videos[i+1].snippet.title}
-                        fullDescription={videos[i+1].snippet.description}
-                    />
-                </div>)}
-
-                {i+2<videos.length&&(
-                <div key={i+2} className="w3-col l4">
-                    <SingleVideoComponent
-                        date={videos[i+2].snippet.publishedAt.split("T")[0]}
-                        link={videos[i+2].id.videoId}
-                        image={videos[i+2].snippet.thumbnails.high.url}
-                        title={videos[i+2].snippet.title}
-                        fullDescription={videos[i+2].snippet.description}
-                    />
-                </div>)}
-            </div>
-        );
-    }
 
     return (
         <>
