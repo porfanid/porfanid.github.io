b846a6383c56c3344c868035b58d0d82406b5f25
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-11-08 11:42:00 +0200

    Finished the page

--- askioannina/src/API/messaging.js
+++ askioannina/src/API/messaging.js
@@ -1,64 +1,42 @@
-import { initializeApp } from 'firebase/app';
-import {getToken,  getMessaging} from 'firebase/messaging';
-import {useState} from "react";
+import axios from 'axios';
 
 
+const viberApiToken = '51e9a60ddf27e10e-170116cc0945c0a-1308888e03cccb00';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCuZ4dAjkNOuCXQKCglCRnUxAPzRjS80g8",
-    authDomain: "pavlos-quantum-loop.firebaseapp.com",
-    projectId: "pavlos-quantum-loop",
-    storageBucket: "pavlos-quantum-loop.appspot.com",
-    messagingSenderId: "156328566952",
-    appId: "1:156328566952:web:5858f21259dec75dd9e29d"
-
-};
-
-
-const messaging_api_key = " BL4Byyt9qRnveVa81DjwsaPkz1jCvmpUsP3K1gCXXquSIBTIumco79w51xGdJcIYYyTA4Ni8fQ-yyEbXr-Uqh2A";
-const firebaseApp = initializeApp(firebaseConfig);
-
-const messaging = getMessaging(firebaseApp);
-
-async function EnableMessaging(callback) {
-    const [isTokenFound, setTokenFound] = useState(false);
-    await getTokenAPI(setTokenFound);
-    if(isTokenFound) {
-        messaging.onBackgroundMessage(function (payload) {
-            console.log('Received background message ', payload);
-
-            const notificationTitle = payload.notification.title;
-            const notificationOptions = {
-                body: payload.notification.body,
-            };
-
-            console.log(notificationTitle);
-            console.log(notificationOptions)
-
-            //self.registration.showNotification(notificationTitle,
-            //    notificationOptions);
-        });
-    }
-}
-
-
-
-export const getTokenAPI = (setTokenFound) => {
-    return getToken(messaging, {vapidKey: messaging_api_key}).then((currentToken) => {
-        if (currentToken) {
-            console.log('current token for client: ', currentToken);
-            setTokenFound(true);
-            // Track the token -> client mapping, by sending to backend server
-            // show on the UI that permission is secured
-        } else {
-            console.log('No registration token available. Request permission to generate one.');
-            setTokenFound(false);
-            // shows on the UI that permission is required
-        }
-    }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // catch error while creating client token
+function send_message(text, userPhoneNumber='306988507680'){
+    //const  = ;
+    axios.get(`https://chatapi.viber.com/pa/get_account_info?phone=${userPhoneNumber}`, {
+        headers: {
+            'X-Viber-Auth-Token': viberApiToken,
+        },
+    })
+    .then((response) => {
+        const userInfo = response.data;
+        const userId = userInfo.id;
+        send_message_to_api(userId, text);
+    })
+    .catch((error) => {
+        console.error('Failed to get user info');
     });
 }
 
-export default EnableMessaging;
+function send_message_to_api(user_id, text) {
+    const contactData = {
+        receiver: user_id, // Replace with the recipient's Viber user ID
+        text: text,
+    };
+    const viberApiUrl = 'https://chatapi.viber.com/pa/send_message';
+    axios.post(viberApiUrl, contactData, {
+        headers: {
+            'X-Viber-Auth-Token': viberApiToken,
+        },
+    })
+    .then((response) => {
+        console.log('Message sent to Viber:', response.data);
+    })
+    .catch((error) => {
+        console.error('Failed to send message to Viber:', error);
+    });
+}
+
+export default send_message;
